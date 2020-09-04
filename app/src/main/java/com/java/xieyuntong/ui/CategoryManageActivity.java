011a257dd84d00a5cf504d003cc614431f962d23
huangqc18 <50017491+huangqc18@users.noreply.github.com>
2020-09-04 15:43:31 +0800

    hqc

---
+++ app/src/main/java/com/java/xieyuntong/ui/CategoryManageActivity.java
@@ -0,0 +1,16 @@
+package com.java.xieyuntong.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.java.xieyuntong.R;
+
+public class CategoryManageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_manage);
+    }
+}
