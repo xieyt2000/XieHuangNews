866aa2636be7ac3dad62c59e0493a852ee9e2a75
huangqc18 <50017491+huangqc18@users.noreply.github.com>
2020-09-05 22:08:36 +0800

    hqc

--- app/src/main/java/com/java/xieyuntong/data/EpidemicDataActivity.java
+++ app/src/main/java/com/java/xieyuntong/data/EpidemicDataActivity.java
@@ -2,7 +2,6 @@ package com.java.xieyuntong.data;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,7 +9,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.java.xieyuntong.R;
+import com.java.xieyuntong.backend.EpidemicStat;
 import com.java.xieyuntong.backend.StatAPI;
 
 import java.util.ArrayList;
@@ -23,23 +28,36 @@ public class EpidemicDataActivity extends AppCompatActivity {
     private String[] allCountries;
     private String[] allProvinces;
     private TextView mTextView;
+    private EpidemicStat epidemicStat;//数据
+    private ArrayList<Integer> ConfirmedCount;
+    private ArrayList<Integer> SuspectedCount;
+    private ArrayList<Integer> CuredCount;
+    private ArrayList<Integer> DeadCount;
+    private ArrayList<Integer> SevereCount;
+    private ArrayList<Integer> RiskCount;
+    private ArrayList<Integer> Inc24Count;
+    private BarChart[] barCharts;
+    private int[] BarChartId = {R.id.bar_chart_confirm,R.id.bar_chart_suspected,R.id.bar_chart_cured,R.id.bar_chart_dead
+    ,R.id.bar_chart_severe,R.id.bar_chart_risk,R.id.bar_chart_inc24};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_epidemic_data);
-        init();
-    }
-
-    private void init() {
         initView();
     }
+
+
 
     private void initView() {
         mCountrySpinner = findViewById(R.id.spinner_country);
         mProvinceSpinner = findViewById(R.id.spinner_province);
         mTextView = findViewById(R.id.country_province_text);
+        barCharts = new BarChart[7];
+        for (int i = 0; i < barCharts.length; i++) {
+            barCharts[i] = findViewById(BarChartId[i]);
+        }
         ArrayList<String> countries = StatAPI.getAllCountries();
         allCountries = (String[]) countries.toArray(new String[countries.size()]);
         ArrayAdapter<String> countryApatper = new ArrayAdapter<String>(this,
@@ -77,6 +95,8 @@ public class EpidemicDataActivity extends AppCompatActivity {
                     s = country + "国"+province+"省"+"统计数据";
                 }
                 mTextView.setText(s);
+                initData();
+                initBarChart();
             }
 
             @Override
@@ -85,5 +105,33 @@ public class EpidemicDataActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void initBarChart() {
+        for(BarChart barChart:barCharts){
+            barChart.getDescription().setEnabled(false); // 不显示描述
+            barChart.setExtraOffsets(20, 20, 20, 20);
+            setAxis(barChart);//设置坐标轴
+            setLegend(barChart);//设置图例
+            setData(barChart);//设置数据
+        }
+    }
+
+    private void setLegend(BarChart barChart) {
+    }
+
+    private void setAxis(BarChart barChart) {
+
+    }
+
+    private void setData(BarChart barChart) {
+    }
+
+    private void initData() {
+        if(province.equals("全国")){//全国数据
+            epidemicStat = StatAPI.getStatByCountry(country);
+        }else{//省份数据
+            epidemicStat = StatAPI.getStatByProvince(country,province);
+        }
     }
 }
